c2386cb0c434d45b4a9983ba6a2e0e4e328da7d4
akash <akash2320@gmail.com>
2025-10-06 15:48:27 -0400

    crop data surfaces to visualize

--- utils/load_data_funcs.py
+++ utils/load_data_funcs.py
@@ -2,7 +2,13 @@ import h5py
 from pydicom import dcmread
 from natsort import natsorted
 import os
+from utils.util_funcs import min_max, resource_path
+from utils.transmorph_helper_funcs import preprocess_img, detect_areas
+import napari
 import numpy as np
+import yaml
+import logging
+import sys
 
 def load_h5_data(dirname, scan_num):
     if dirname.endswith(('.h5','.hdf5')):
@@ -66,3 +72,39 @@ def GUI_load_h5(path_h5):
         original_data = hf['volume'][:,:,:]
     return original_data
 
+def load_napari_viewer(data):
+    config_path = 'datapaths.yaml'
+    try:
+        with open(resource_path(config_path), 'r') as f:
+            config = yaml.safe_load(f)
+    except:
+        with open(config_path, 'r') as f:
+            config = yaml.safe_load(f)
+    try:
+        from ultralytics import YOLO
+        MODEL_FEATURE_DETECT_PATH = config['PATHS']['MODEL_FEATURE_DETECT_PATH']
+        MODEL_FEATURE_DETECT = YOLO(MODEL_FEATURE_DETECT_PATH)
+        logging.info("YOLO Model Loaded Succesfully.")
+        # if self.is_gui_flag:
+        #     self.output_queue.put("YOLO Model Loaded Succesfully.\n")
+    except Exception as e:
+        logging.error(f"Error loading YOLO model: {e}", exc_info=True)
+        # sys.exit("Failed to load YOLO model. Exiting.")
+
+    # Detection part
+    static_flat = np.argmax(np.sum(data[:,:,:], axis=(0,1)))
+    test_detect_img = preprocess_img(data[:,:,static_flat])
+    res_surface = MODEL_FEATURE_DETECT.predict(test_detect_img, iou=0.2, save=False, max_det = 100, # consider 100 as max
+                                                verbose=False, classes = 0, device='cpu', agnostic_nms=True, augment=True)
+    surface_crop_coords = detect_areas(res_surface[0].summary(), pad_val = 30, # 30 pixels padding, can be changed
+                                        img_shape=test_detect_img.shape[0], expected_num=100) # consider 100 as max
+    # print(surface_crop_coords)
+    view_data = (min_max(data)*255).astype(np.uint8)
+    viewer = napari.Viewer()
+    viewer.add_image(data = view_data, name = 'whole data')
+    for idx, (i_cord, j_cord) in enumerate(surface_crop_coords):
+        temp_crop_data = data[:, i_cord:j_cord, :]
+        temp_crop_data = (min_max(temp_crop_data)*255).astype(np.uint8)
+        viewer.add_image(data = temp_crop_data, name = f'crop {idx}', visible = False)
+    del data, view_data, temp_crop_data
+    return viewer
